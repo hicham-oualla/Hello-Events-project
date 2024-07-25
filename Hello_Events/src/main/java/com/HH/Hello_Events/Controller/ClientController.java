b8a304636a2b5bdfab6f8542d9687bd3a4c4a882
hicham-oualla <hich2022am.oualla@gmail.com>
2024-07-25 10:27:07 +0100

    client conroller

---
+++ Hello_Events/src/main/java/com/HH/Hello_Events/Controller/ClientController.java
@@ -0,0 +1,27 @@
+package com.HH.Hello_Events.Controller;
+
+import com.HH.Hello_Events.Model.Entity.Client;
+import com.HH.Hello_Events.Services.ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/client")
+public class ClientController {
+
+    @Autowired
+    private ClientService clientService;
+
+
+    @GetMapping("/allclients")
+    public List<Client> getAllClients() {
+        return clientService.getAllclients();
+    }
+    @DeleteMapping("/delete/{id}")
+    public void deleteClient(@PathVariable int id) {
+        clientService.deleteClient(id);
+    }
+
+}
