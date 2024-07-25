831b0cdbdd18571a1a98a0637cd2071d869ac4d4
hicham-oualla <hich2022am.oualla@gmail.com>
2024-07-25 10:32:23 +0100

    ApplicationConfig:

--- Hello_Events/src/main/java/com/HH/Hello_Events/config/SpringsecurityConfig.java
+++ Hello_Events/src/main/java/com/HH/Hello_Events/config/SpringsecurityConfig.java
@@ -53,7 +53,7 @@
 //
 
 
-package com.app.e_bank.solution.config;
+package com.HH.Hello_Events.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -62,8 +62,6 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -83,6 +81,7 @@ public class SpringsecurityConfig {
         http.csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/api/v1/auth/**").permitAll()
+                        .requestMatchers("/api/client/**").permitAll()
                         .anyRequest()
                         .authenticated()
                 )
