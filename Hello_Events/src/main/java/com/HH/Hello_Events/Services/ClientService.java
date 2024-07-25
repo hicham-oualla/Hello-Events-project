c0defa8618033908ea56fb24af31f146f3dd2c61
hicham-oualla <hich2022am.oualla@gmail.com>
2024-07-25 10:26:33 +0100

    creation de interface et implementation des methode

--- Hello_Events/src/main/java/com/HH/Hello_Events/Services/ClientService.java
+++ Hello_Events/src/main/java/com/HH/Hello_Events/Services/ClientService.java
@@ -1,2 +1,11 @@
-package com.HH.Hello_Events.Services;public interface ClientService {
+package com.HH.Hello_Events.Services;
+
+import com.HH.Hello_Events.Model.Entity.Client;
+
+import java.util.List;
+
+public interface ClientService {
+    List<Client> getAllclients();
+    void deleteClient(int id);
+
 }
