897401fecb804bc1bd8ec5a08cf88a39d0b33fdd
hicham-oualla <hich2022am.oualla@gmail.com>
2024-07-26 11:49:56 +0100

    ajouter method count

--- Hello_Events/src/main/java/com/HH/Hello_Events/Repository/EventRepository.java
+++ Hello_Events/src/main/java/com/HH/Hello_Events/Repository/EventRepository.java
@@ -2,7 +2,12 @@ package com.HH.Hello_Events.Repository;
 
 import com.HH.Hello_Events.Model.Entity.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface EventRepository extends JpaRepository<Event,Integer> {
+
+    @Query("SELECT COUNT  (*) from  Event ")
+    long countEvent();
+
 
 }
