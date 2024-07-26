91820c343a2a464a00194f267a1463b2993d06d3
hicham-oualla <hich2022am.oualla@gmail.com>
2024-07-26 14:17:59 +0100

    creatio de contact repository

---
+++ Hello_Events/src/main/java/com/HH/Hello_Events/Repository/ContactRepository.java
@@ -0,0 +1,7 @@
+package com.HH.Hello_Events.Repository;
+
+import com.HH.Hello_Events.Model.Entity.Contact;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ContactRepository  extends JpaRepository<Contact, Integer> {
+}
