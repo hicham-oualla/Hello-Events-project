d3010dae63cf02ba3e4703413efdb0df62821cb3
hicham-oualla <hich2022am.oualla@gmail.com>
2024-07-27 14:01:32 +0100

    contact repository

--- Hello_Events/src/main/java/com/HH/Hello_Events/Repository/ContactRepository.java
+++ Hello_Events/src/main/java/com/HH/Hello_Events/Repository/ContactRepository.java
@@ -1,7 +1,7 @@
 package com.HH.Hello_Events.Repository;
-
 import com.HH.Hello_Events.Model.Entity.Contact;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ContactRepository  extends JpaRepository<Contact, Integer> {
+public interface ContactRepository extends JpaRepository<Contact, Integer> {
+
 }
