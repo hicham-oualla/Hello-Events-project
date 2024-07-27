858bb79d15b4852359f42f413a888e776afaf6ea
hicham-oualla <hich2022am.oualla@gmail.com>
2024-07-27 14:02:33 +0100

    contactService & contactimpl

---
+++ Hello_Events/src/main/java/com/HH/Hello_Events/Services/ContactServiceImpl.java
@@ -0,0 +1,44 @@
+package com.HH.Hello_Events.Services;
+import com.HH.Hello_Events.Model.Entity.Contact;
+import com.HH.Hello_Events.Repository.ContactRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ContactServiceImpl implements Contactservice {
+    @Autowired
+    private ContactRepository contactRepository;
+
+    @Override
+    public Contact createContact(Contact contact) {
+        return contactRepository.save(contact);
+    }
+
+    @Override
+    public Contact getContactById(int id) {
+        Optional<Contact> optionalContact = contactRepository.findById(id);
+        return optionalContact.orElse(null);
+    }
+
+    @Override
+    public List<Contact> getAllContacts() {
+        return contactRepository.findAll();
+    }
+
+    @Override
+    public Contact updateContact(int id, Contact contact) {
+        if (contactRepository.existsById(id)) {
+            contact.setId(id);
+            // No need to fetch the old contact, just save the new one
+            return contactRepository.save(contact);
+        }
+        return null;
+    }
+
+    @Override
+    public void deleteContact(int id) {
+        contactRepository.deleteById(id);
+    }
+}
---
+++ Hello_Events/src/main/java/com/HH/Hello_Events/Services/Contactservice.java
@@ -0,0 +1,13 @@
+package com.HH.Hello_Events.Services;
+
+import com.HH.Hello_Events.Model.Entity.Contact;
+
+import java.util.List;
+
+public interface Contactservice {
+    Contact createContact(Contact contact);
+    Contact getContactById(int id);
+    List<Contact> getAllContacts();
+    Contact updateContact(int id, Contact contact);
+    void deleteContact(int  id);
+}
