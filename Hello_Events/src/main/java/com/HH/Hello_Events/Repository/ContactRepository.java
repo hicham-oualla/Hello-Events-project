e8766f278a13e68002767b6174cf42ab3e13334d
hicham-oualla <hich2022am.oualla@gmail.com>
2024-07-27 16:30:30 +0100

    ajouter une requet pour cacluler nbr de contact

--- Hello_Events/src/main/java/com/HH/Hello_Events/Repository/ContactRepository.java
+++ Hello_Events/src/main/java/com/HH/Hello_Events/Repository/ContactRepository.java
@@ -1,7 +1,9 @@
 package com.HH.Hello_Events.Repository;
 import com.HH.Hello_Events.Model.Entity.Contact;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ContactRepository extends JpaRepository<Contact, Integer> {
-
+    @Query("SELECT COUNT  (*) from      Contact ")
+    long countContact();
 }
