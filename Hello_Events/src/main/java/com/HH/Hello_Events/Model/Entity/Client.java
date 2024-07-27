ebac16e126347cf4274e117f0846f40261dfd3ae
hamzahamzahamzahm <hamzahamichan159@gmail.com>
2024-07-27 13:23:18 +0100

    ajouter des modification

--- Hello_Events/src/main/java/com/HH/Hello_Events/Model/Entity/Client.java
+++ Hello_Events/src/main/java/com/HH/Hello_Events/Model/Entity/Client.java
@@ -1,5 +1,6 @@
 package com.HH.Hello_Events.Model.Entity;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
@@ -17,7 +18,7 @@ import java.util.List;
 public class Client implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private int id_client;
     private String fullName;
     private String email;
     private String password;
@@ -26,6 +27,7 @@ public class Client implements UserDetails {
 
 
     @OneToMany(mappedBy ="client")
+    @JsonManagedReference
     private List<Billet>billets;
 
     @Override
