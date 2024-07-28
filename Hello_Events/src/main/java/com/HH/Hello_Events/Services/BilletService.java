98b742d6ac56dd896d702b20c27b501dfb3540fc
hamzahamzahamzahm <hamzahamichan159@gmail.com>
2024-07-28 09:31:27 +0100

    Service interface

---
+++ Hello_Events/src/main/java/com/HH/Hello_Events/Services/BilletService.java
@@ -0,0 +1,14 @@
+package com.HH.Hello_Events.Services;
+
+import com.HH.Hello_Events.Model.Entity.Billet;
+import com.HH.Hello_Events.Model.Entity.Client;
+import com.HH.Hello_Events.Model.Entity.Event;
+
+import java.util.Optional;
+
+
+public interface BilletService {
+
+    Billet addBillet(Billet billet);
+
+}
